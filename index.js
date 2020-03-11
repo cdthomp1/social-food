92a1e8f7ffb7d6e7740dd358b1e7644ce51b11ff
cdthomp1 <tho17015@byui.edu>
2020-03-10 22:34:10 -0600

    starter

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+var app = express();
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => res.render('pages/index'))
+app.get('/login', (req, res) => res.render('pages/login'))
+app.get('/register', (req, res) => res.render('pages/register'))
+app.get('/public', (req, res) => res.render('pages/public'))
+app.get('/personal', (req, res) => res.render('pages/personal'))
+app.listen(PORT, () => console.log(`Listening on ${PORT}`))
