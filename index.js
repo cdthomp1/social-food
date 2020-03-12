a6f321a91651ca1c6bcffdde576f7314178ba9e0
cdthomp1 <tho17015@byui.edu>
2020-03-11 23:30:04 -0600

    test

--- index.js
+++ index.js
@@ -42,9 +42,8 @@ function getUsers(email, password) {
       console.log("Error in query: ")
       console.log(err);
     } else {
-      console.log("Found result: " + JSON.stringify(result.rows))
       var res = JSON.stringify(result.rows);
-
+      console.log(res);
       console.log(typeof(res));
     }
 
