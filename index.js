db9aa80c6d5fe9244c849e99ffdf75345b064ae3
cdthomp1 <tho17015@byui.edu>
2020-03-13 17:02:56 -0600

    test

--- index.js
+++ index.js
@@ -14,20 +14,18 @@ const recipes = require('./api/recipe-api');
 var app = express();
 
 app.use(express.json());
-/* app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.get('/', (req, res) => res.render('pages/index'))
 app.get('/login', (req, res) => { res.render('pages/login')})
 app.get('/register', (req, res) => res.render('pages/register'))
 app.get('/public', (req, res) => res.render('pages/public'))
-app.get('/personal', (req, res) => res.render('pages/personal')) */
+app.get('/personal', (req, res) => res.render('pages/personal'))
 
 //API 
-
 app.use('/api/v1/users', users);
 app.use('/api/v1/recipes', recipes);
-
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`))
 
