51fe44f6aeda72da13ee62965267322f656a2410
cdthomp1 <tho17015@byui.edu>
2020-03-14 17:10:34 -0600

    Major Update

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MogoURI: 'mongodb+srv://cameronthompson:304439-ad@transactions-pez1f.mongodb.net/Social-food?retryWrites=true&w=majority'
+}
--- models/User.js
+++ models/User.js
@@ -1,26 +1,27 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        trim: true,
-        required: [true, 'Please Enter Your Name']
-    },
-    email: {
-        type: String,
-        required: [true, 'Please Enter Email']
-    },
-    password: {
-        type: String,
-        required: [true, "Please Enter Password"]
-    },
-    favRecipes : {
-        type: [{}]
-    },
-    createdAt: {
-        type: Date,
-        defualt: Date.now
-    }
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  favRecs: {
+    type: Array
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
 });
 
-module.exports = mongoose.model('User', UserSchema)
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
