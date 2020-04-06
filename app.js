015d73e71feda9aed606aeb2948bc80544246be7
cdthomp1 <tho17015@byui.edu>
2020-04-06 09:33:27 -0700

    update

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const flash = require('connect-flash');
 const session = require('express-session');
 const path = require('path');
 
+const GridFsStorage = require('multer-gridfs-storage');
+const Grid = require('gridfs-stream');
 
 const app = express();
 
@@ -66,5 +68,40 @@ app.use('/users', require('./routes/users.js'));
 app.use('/api/v1', require('./api/api'));
 
 const PORT = process.env.PORT || 7472;
+
+
+
+// Init gfs
+let gfs;
+const conn = mongoose.createConnection(db);
+
+conn.once('open', () => {
+  // Init stream
+  gfs = Grid(conn.db, mongoose.mongo);
+  gfs.collection('uploads');
+});
+
+app.get('/image/:filename', (req, res) => {
+  gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+    // Check if file
+    if (!file || file.length === 0) {
+      return res.status(404).json({
+        err: 'No file exists'
+      });
+    }
+    
+    // Check if image
+    if (file.contentType === 'image/jpeg' || file.contentType === 'image/png') {
+      // Read output to browser
+      const readstream = gfs.createReadStream(file.filename);
+      readstream.pipe(res);
+    } else {
+      res.status(404).json({
+        err: 'Not an image'
+      });
+    }
+  });
+});
+
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
